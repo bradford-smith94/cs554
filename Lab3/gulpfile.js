78eadd592d7f5270610d6c64f0049c46a77d37af
Bradford Smith <bsmith8@stevens.edu>
2017-09-16 16:24:40 -0400

    Add gulpfile, server and some basic sass
    This sets up most of what gulp needs to do as well as adds the server
    from Lab 2.

---
+++ Lab3/gulpfile.js
@@ -0,0 +1,36 @@
+/* Bradford Smith (bsmith8)
+ * CS 554 Lab 3 gulpfile.js
+ * 09/16/2017
+ */
+
+const gulp = require("gulp");
+const gulpSASS = require("gulp-sass");
+const concatenate = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const autoPrefix = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+
+const sassFiles = [
+    "./src/styles/variables.scss",
+    "./src/styles/custom.scss"
+];
+
+gulp.task("sass", () => {
+    gulp.src(sassFiles)
+        .pipe(concatenate("styles.scss"))
+        .pipe(gulpSASS())
+        .pipe(gulp.dest("./public/css/"))
+        .pipe(autoPrefix({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename("styles.min.css"))
+        .pipe(gulp.dest("./public/css/"));
+});
+
+gulp.task("watch", () => {
+    gulp.watch(sassFiles, ["sass"]);
+});
+
+gulp.task("default", ["sass", "watch"]);
