05bc1a0ac14e6af8ed1f2a64da2c27cbae4b901a
Bradford Smith <bsmith8@stevens.edu>
2017-10-14 15:18:22 -0400

    Simplify axios config
    There is no need to add an interceptor because the PokeAPI is so simple.
    All we need to do is specify the axios 'baseURL'.

--- Lab4/src/utils/axiosInstance.js
+++ Lab4/src/utils/axiosInstance.js
@@ -2,13 +2,6 @@ import axios from 'axios';
 
 const apiRoot = 'https://pokeapi.co/api/v2/';
 
-const instance = axios.create();
-
-instance.interceptors.request.use(function (config) {
-  config.url = `${apiRoot}${config.url}`;
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+const instance = axios.create({ baseURL: apiRoot });
 
 export default instance;
