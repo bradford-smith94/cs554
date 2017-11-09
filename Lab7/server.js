b2f5963ee7af6910c02aea5dfdb6ba6dc3fd4ace
Bradford Smith <bsmith8@stevens.edu>
2017-11-09 17:23:27 -0500

    Setup basic socket communication
    Needed to specify 'socket.io@1.2.0' because that is the CDN version I
    have. This gets the form communicating with the server over a socket.
    
    Also moved to statically hosting anything in `/public` in order to get
    CSS working for the index page.

--- Lab7/server.js
+++ Lab7/server.js
@@ -1,19 +1,31 @@
-/* Bradford Smith
+/* Bradford Smith (bsmith8)
  * CS 554 Lab 7 server.js
  * 11/09/2017
  */
 
-const app = require('express')();
-const http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const search = io.of('/search');
 const usersToSocket = {};
 
-app.get('/', function (request, response) {
-    response.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.use('*', function (request, response) {
     response.status(404).sendFile(__dirname + '/404.html');
+});
+
+search.on('connection', function (socket) {
+    socket.on('request', function (request) {
+        if (usersToSocket[request.name] &&
+            usersToSocket[request.name] !== socket) {
+            socket.emit('name-fail');
+        } else {
+            usersToSocket[request.name] = socket;
+            socket.emit('name-good');
+        }
+    });
 });
 
 http.listen(3000, function () {
